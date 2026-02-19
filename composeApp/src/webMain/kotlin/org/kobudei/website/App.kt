705a77a566b9d8436c9a302805c7b2d9f785646b
Viorel Alexandrescu <viorel.alexandrescu@kobudei.org>
2026-02-19 16:27:31 +0200

    added work in progress banner

--- composeApp/src/webMain/kotlin/org/kobudei/website/App.kt
+++ composeApp/src/webMain/kotlin/org/kobudei/website/App.kt
@@ -2,18 +2,11 @@ package org.kobudei.website
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import website.composeapp.generated.resources.Res
-import website.composeapp.generated.resources.mail_24
 
 @Composable
 fun App() {
@@ -25,6 +18,20 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = androidx.compose.ui.graphics.Color.Yellow)
+                    .safeContentPadding(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "🚧 Work in Progress 🚧",
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
             Row(modifier = Modifier
                 .background(MaterialTheme.colorScheme.primary)
                 .fillMaxWidth()) {

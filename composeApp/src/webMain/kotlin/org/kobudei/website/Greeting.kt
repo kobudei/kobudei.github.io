6c435f55b222cb2f6f11d23ffe7f47797330d79a
Viorel Alexandrescu <viorel.alexandrescu@kobudei.org>
2026-02-20 13:52:58 +0200

    cleaned up generated classes

--- composeApp/src/webMain/kotlin/org/kobudei/website/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.kobudei.website
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello World! This app is for ${platform.name}!"
-    }
-}

89bb954f2ca39640c8544d1c6fa003480a5fe132
Viorel Alexandrescu <viorel.alexandrescu@kobudei.org>
2026-03-04 07:41:37 +0200

    changed contact button to display only an icon and show a tooltip on hover

--- composeApp/src/webMain/kotlin/org/kobudei/website/ContactButton.kt
+++ composeApp/src/webMain/kotlin/org/kobudei/website/ContactButton.kt
@@ -1,9 +1,14 @@
 package org.kobudei.website
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.resources.painterResource
 import website.composeapp.generated.resources.Res
@@ -16,20 +21,30 @@ expect fun openEmail(
     body: String
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactButton() {
     MaterialTheme {
-        FilledTonalButton(
-            onClick = {
-                openEmail(
-                    to = "contact@kobudei.org",
-                    subject = "Hello!",
-                    body = "Hello Kobudei community! My name is..."
-                )
-            }
+        TooltipBox(
+            positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+            tooltip = {
+                PlainTooltip { Text("Contact us!") }
+            },
+            state = rememberTooltipState()
         ) {
-            Icon(painter = painterResource(Res.drawable.mail_24), null)
-            Text("Contact Us!")
+            FilledTonalButton(
+                onClick = {
+                    openEmail(
+                        to = "contact@kobudei.org",
+                        subject = "Hello!",
+                        body = "Hello Kobudei community! My name is..."
+                    )
+                }
+            ) {
+                Icon(
+                    painter = painterResource(Res.drawable.mail_24),
+                    contentDescription = "Contact")
+            }
         }
     }
 }
